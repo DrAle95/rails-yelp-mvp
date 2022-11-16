06879a4ac05ab17ae0a6ce966eba24a829a1697e
Alexey Lebedev <alexeylebedev1995@gmail.com>
2022-11-16 20:03:52 +0200

    dsadsd

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   resources :reviews, only: [:destroy]
 
   # get 'restaurants', to: 'restaurants#index'
+
+  # get 'restaurants/top', to: 'restaurants#top', as: 'top_restaurants' # # get "/restaurants/top", to: "restaurants#top", as: "top_restaurants"
+  # get 'restaurants/:id/chef', to: 'restaurants#chef' # GET /restaurants/:id/chef
+
   # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
   # post 'restaurants', to: 'restaurants#create'
 
